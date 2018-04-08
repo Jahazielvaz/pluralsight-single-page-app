83df0fd4ef78b4fc564519f3395b8e5f63bfcdfe
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2018-04-07 18:42:31 -0600

    Environment fully set up

---
+++ server/main.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', function(req, res){
+  res.render('../app/index.ejs')
+})
+app.use(express.static(__dirname + "/../.tmp"))
+app.listen(7777);
